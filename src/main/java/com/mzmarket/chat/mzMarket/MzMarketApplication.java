91b10a41ed661bec356df5d9c8c6e6b6dafe82b1
hyunjun_cho <hyunjun_cho3@tmax.co.kr>
2023-01-23 16:04:04 +0900

    chore: project setup

---
+++ src/main/java/com/mzmarket/chat/mzMarket/MzMarketApplication.java
@@ -0,0 +1,13 @@
+package com.mzmarket.chat.mzMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MzMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mzmarket/chat/mzMarket/MzMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzmarket.chat.mzMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
